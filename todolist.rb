8e8eb8b2bd88152a635d94a8f3621066ee6afe4f
Xiaohu Li <li.xiaohu6@gmail.com>
2016-06-09 20:59:27 -0700

    main features

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,40 @@
 class TodoList
     # methods and stuff go here
+  attr_accessor :title, :items
+
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new
+  end
+
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
+  end
+
+  def remove (position)
+    @items.delete_at (position)
+  end
+
+  def print
+    puts "#{self.title}:"
+    @items.each do |item|
+      puts "#{item.description} --> #{item.status}"
+    end
+  end
+
+  def complete (position)
+    @items[position].status = true
+  end
 end
 
 class Item
     # methods and stuff go here
+  attr_accessor :description, :status
+
+  def initialize(description)
+    @description = description
+    @status = false
+  end
+
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,27 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
-
+tasks = TodoList.new "tasks for today"
 # Add four new items
-
+tasks.add_item "appointment with doctor"
+tasks.add_item "cook dinner"
+tasks.add_item "study ruby"
+tasks.add_item "work out"
 # Print the list
-
+tasks.print
 # Delete the first item
-
+tasks.remove 0
 # Print the list
-
+tasks.print
 # Delete the second item
-
+tasks.remove 1
 # Print the list
-
+tasks.print
 # Update the completion status of the first item to complete
-
+tasks.complete 0
 # Print the list
-
+tasks.print
 # Update the title of the list
-
+tasks.title = "tasks for tomorrow"
 # Print the list
+tasks.print
